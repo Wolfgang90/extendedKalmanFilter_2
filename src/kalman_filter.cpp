839495653a458abea2c1d353d3990212125e43c7
Wolfgang90 <eichler_wolfgang@msn.com>
2017-04-13 08:27:35 +0200

    Radar Kalman Filter implemented

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -33,11 +33,48 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
-}
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd H_T = H_.transpose();
+  MatrixXd S = H_ * P_ * H_T + R_;
+  MatrixXd S_I = S.inverse();
+  MatrixXd PH_T = P_ * H_T;
+  MatrixXd K = PH_T * S_I;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+
+  float ro_pred = pow(pow(px,2)+pow(px,2), 0.5);
+  float phi_pred = atan2(py, px);
+  float ro_dot = (px * vx + py * vy) / ro_pred;
+
+  VectorXd z_pred(3);
+  z_pred << ro_pred, phi_pred, ro_dot;
+
+  VectorXd y = z -z_pred;
+  MatrixXd H_T = H_.transpose();
+
+  MatrixXd S = H_ * P_ * H_T + R_;
+  MatrixXd S_I = S.inverse();
+  MatrixXd PH_T = P_ * H_T;
+  MatrixXd K = PH_T * S_I;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
